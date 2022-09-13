b76177434170dbe2b3cd87e6c370fe7b8fe04f22
MajorFemi <harlaohorluwafemi@gmail.com>
2022-09-13 16:47:29 -0700

    third commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,11 +2,11 @@
 /**
  * main - Entry point or main body
  *
- * Description: Prints the alphabets in lowercase
+ * Description: print_alphabet - prints alphabet in lowercase
  *
  * Return: 0
 */
-void print_alphabet(void);
+void print_alphabet(void)	/* prints alphabet */
 {
 	char lower = 'a';
 
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,7 +2,7 @@
 #define MAIN_FILE
 
 int _putchar(char f);
-
 void print_alphabet(void);
+void print_alphabet_x10(void);
 
 #endif
