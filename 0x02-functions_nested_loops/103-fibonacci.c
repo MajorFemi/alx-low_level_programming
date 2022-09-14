1090d7a91bd7a49f8ede39b5568e90502de71ace
MajorFemi <harlaohorluwafemi@gmail.com>
2022-09-13 19:24:54 -0700

    sixteenth commit

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main -main body or Entry point
+ *
+ * Description: Print the sum of even fibonacci numbers up to a fib value
+ * not exceeding 4,000,000
+ * Return: 0
+ */
+int main(void)
+{
+	int f = 1, e = 2, sum = 0;
+	int m;
+
+	while (e < 4000000)
+	{
+		if (e % 2 == 0)
+			total += e;
+
+		m = e;
+		e += f;
+		f = m;
+	}
+	printf("%d\n", total);
+
+	return (0);
+}
