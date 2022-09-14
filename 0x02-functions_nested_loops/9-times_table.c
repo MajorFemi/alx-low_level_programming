5764f365088ed6acd6f284073efe003f8a3b9f47
MajorFemi <harlaohorluwafemi@gmail.com>
2022-09-13 18:24:57 -0700

    tenth commit

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * times_table - prints the 9 times table
+ * Description: tablr show start from 0
+ *
+ * Return: 0
+ */
+void times_table(void)
+{
+	int f = 0;
+	int e;
+	int m;
+
+	while (f < 10)
+	{
+		e = 0;
+		while (e < 10)
+		{
+			m = f * e;
+
+			if (m > 9)
+			{
+				_putchar(m / 10 + '0');
+				_putchar(m % 10 + '0');
+			}
+			else if (e != 0)
+			{
+				_putchar(' ');
+				_putchar(m + '0');
+			}
+			else
+			{
+				_putchar(m + '0');
+			}
+
+			if (e != 9)
+			{
+				_putchar(44);
+				_putchar(' ');
+			}
+			e++
+		}
+		_putchar('\n');
+		f++
+	}
+}
