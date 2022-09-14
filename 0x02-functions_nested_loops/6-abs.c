6e09aefcc9ca68ecbf670fc4466cd9f0b05c6a4a
MajorFemi <harlaohorluwafemi@gmail.com>
2022-09-13 17:35:23 -0700

    seventh commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ * @f: int type number
+ * Return: absolute value of @f
+ */
+int _abs(int f)
+{
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	else
+	{
+		return (n);
+	}
+}
