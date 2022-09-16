4401fc908a48982c1bee197b0575e244fa0ff2ce
MajorFemi <harlaohorluwafemi@gmail.com>
2022-09-15 17:30:53 -0700

    fifth commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,6 +3,7 @@
 /**
  * print_most_numbers - prints the number from 0 to 9, except 2 and 4
  * Description: can only use _putchar twice
+ * Return: void
  */
 
 void print_most_numbers(void)
@@ -11,7 +12,7 @@ void print_most_numbers(void)
 
 	for (f = 0; f < 10; f++)
 	{
-		if (f != '2' && f != '4')
+		if (f != 2 && f != 4)
 		{
 			_putchar(f + '0');
 		}
