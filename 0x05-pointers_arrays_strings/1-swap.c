60e123748f480612e596bacb116d09f128cad278
MajorFemi <harlaohorluwafemi@gmail.com>
2022-09-19 15:50:40 -0700

    second commit

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the value of two integers
+ * @a: pointer one
+ * @b: pointer two
+ */
+
+void swap_int(int *a, int *b)
+{
+	int f = *a;
+
+	*a = *b;
+	*b = x;
+}
