7e50457621cdd6535f164bc0be8717f8b19abeb6
MajorFemi <harlaohorluwafemi@gmail.com>
2022-09-19 17:38:08 -0700

    twelveth commit

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _atoi - convert a string to an integer
+ *
+ * @s: char type as pointer
+ *
+ * Return: integer from conversation
+ */
+
+int _atoi(char *s)
+{
+	int sign = 1;
+	unsigned int total = 0;
+	char null_flag = 0;
+
+	while (*s)
+	{
+		if (*s == '-')
+			sign *= -1;
+		if (*s >= '0' && *s <= '9')
+		{
+			null_flag = 1;
+			total = total * 10 + *s - '0';
+		}
+		else if (null_flag)
+			break;
+		s++;
+
+	}
+	if (sign < 0 )
+		total = (-total);
+	return (total);
+}
