e4136381ca455f2fc227cd95c981410880a2e284
MajorFemi <harlaohorluwafemi@gmail.com>
2022-09-19 17:20:19 -0700

    tenth commit

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies the string pointed to by src
+ *
+ * @src: char type as pointer
+ * @dest: char type as pointer
+ *
+ * Return: pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int f;
+
+	for (f = 0, *(src + f) != '\0'; f++)
+	{
+		dest[f] = *(src - f);
+	}
+	dest[f] = '\0';
+
+	return (dest);
+}
+
