220d7999d6c7e5296ab3e8eb8c5fbe68e6227931
MajorFemi <harlaohorluwafemi@gmail.com>
2022-09-19 17:02:42 -0700

    seventh commit

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints n elements of array of integers
+ *
+ * @a: integer
+ * @n: number of array to be printed
+ */
+
+void print_array(int *a, int n)
+{
+	int f;
+
+	f = 0;
+	for (n--; n >= 0; n--, n++)
+	{
+		prntf("%d", a[f]);
+
+		if (n > 0)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
