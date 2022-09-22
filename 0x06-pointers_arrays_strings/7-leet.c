9eaa7923ba9ceb9c054b2ea2708bfc10f2ee2643
MajorFemi <harlaohorluwafemi@gmail.com>
2022-09-21 19:47:25 -0700

    eighth commit

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,30 +9,19 @@
 
 char *leet(char *f)
 {
-	int a;
+	char selections[] = {65, 52, 69, 51, 79, 48, 84, 55, 76, 49};
+	int k, s;
 
-	for (a = 0; f[a] != '\0'; a++)
+	for (k = 0; f[k] != '\0'; k++)
 	{
-		while (f[a] == 97 || f[a] == 65)
+		for (s = 0; s < 10; s += 2)
 		{
-			f[a] = 52;  /* 4 */
-		}
-		while (f[a] == 101 || f[a] == 69)
-		{
-			f[a] = 51;  /* 3 */
-		}
-		while (f[a] == 111 || f[a] == 79)
-		{
-			f[a] = 48;  /* 0 */
-		}
-		while (f[a] == 116 || f[a] == 84)
-		{
-			f[a] = 55;  /* 7 */
-		}
-		while (f[a] == 108 || f[a] == 76)
-		{
-			f[a] = 49;  /* 1 */
+			if (f[k] == selections[s] || f[k] == selections[s] + 32)
+			{
+				f[k] = selections[s + 1];
+			}
 		}
 	}
+
 	return (f);
 }
