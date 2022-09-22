279c9b0b220c547822f2fcb31d975b01c3cc7364
MajorFemi <harlaohorluwafemi@gmail.com>
2022-09-21 17:41:14 -0700

    fourth commit

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ *
+ * Description: created function works exactly like strcmp
+ *
+ * @s1: pointer one as string
+ * @s2: pointer two as string
+ *
+ * Return: difference between two string
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2)
+	{
+		if (*s1 != *s2)
+		{
+			return (*s1 - *s2);
+		}
+		s1++, s2++;
+	}
+	return (*s1 - *s2);
+}
