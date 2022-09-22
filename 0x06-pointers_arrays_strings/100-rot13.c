c5801f8cb96987876d5712eae00d5912cfb107b9
MajorFemi <harlaohorluwafemi@gmail.com>
2022-09-21 20:13:11 -0700

    ninth commit

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -15,14 +15,13 @@ char *rot13(char *f)
 	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	char cipher[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
-
 	for (s = 0; f[s] != '\0'; s++)
 	{
 		for (k = 0; alphabet[k] != '\0'; k++)
 		{
 			if (f[s] == alphabet[k])
 			{
-				f[s] += cipher[k];
+				f[s] = cipher[k];
 				break;
 			}
 		}
