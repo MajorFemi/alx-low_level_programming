08e7cc8d66da8de6a5a1e8513a0f4a045523323b
MajorFemi <harlaohorluwafemi@gmail.com>
2022-09-26 14:55:53 -0700

    second commit

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies from initial memory area to another
+ * @dest: final memory area
+ * @src: initial memory area
+ * @n: number of byte
+ * Return: a pointer to final memory area
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int f;
+
+	for (f = 0; f < n; f++)
+	{
+		dest[f] = src[n];
+	}
+
+	return (dest);
+}
