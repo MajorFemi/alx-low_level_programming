7336c35b8e2e331f0c0517c9a4900a9a0b5942ff
MajorFemi <harlaohorluwafemi@gmail.com>
2022-09-28 16:14:14 -0700

    first commit

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,13 +9,14 @@
 
 void _puts_recursion(char *s);
 {
-	if (*s == '\0')
+	if (*s != '\0')
 	{
-		_putchar('\n');
+		_putchar(*s);
+		_puts_recursion(++s)
 	}
 	else
 	{
-		_putchar(*s);
-		_puts_recursion(++s);
+		_putchar('\n');
+		return;
 	}
 }
