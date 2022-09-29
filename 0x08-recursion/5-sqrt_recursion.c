18ab079c61c6d5716b89170403314d79a77907af
MajorFemi <harlaohorluwafemi@gmail.com>
2022-09-28 17:04:56 -0700

    sixth commit

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - natural square root of a number
+ * @n: int type
+ *
+ * Return: 1 else -1 if n does not have natural root.
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (sqrt(n, 1));
+}
+
+/**
+ * sqrt - helps to solve _sqrt_recursion
+ * @f: determines if square root
+ * @m: incrementer to compare against 'f'
+ * Return: square root if natural root else -1 if not found
+ */
+
+int sqrt(int f, int m)
+{
+	int square;
+
+	square = m * m;
+	if (square == f)
+	{
+		return (m);
+	}
+	else if (square < m)
+	{
+		return (sqrt(m, f + 1));
+	}
+	else
+		return (-1);
+}
