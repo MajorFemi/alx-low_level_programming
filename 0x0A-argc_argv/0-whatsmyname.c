3372725f71e29e3fc7fed92aff7ad3d7b8b316a0
MajorFemi <harlaohorluwafemi@gmail.com>
2022-09-30 15:42:15 -0700

    initial commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print the name of the file
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
