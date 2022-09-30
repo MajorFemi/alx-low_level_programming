b709a14ae99b369be817e565ed1ee3065e97b1a5
MajorFemi <harlaohorluwafemi@gmail.com>
2022-09-30 16:32:25 -0700

    fourth commit

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - add two positive numbers
+ * @argc: argument count
+ * @argv: argument vector
+ * Description: contains only positive digits
+ * Return: 0 Success else 1 Error
+ */
+
+int main(int argc, char *argv[])
+{
+	int sum, f;
+	char *m;
+	int digit;
+
+	sum = 0;
+	if (argc > 1)
+	{
+		for (f = 1, argv[f]; f++)
+		{
+			digit = strtol(argv[f], &m, 10);
+			if (!*m)
+			{
+				sum += digit;
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
