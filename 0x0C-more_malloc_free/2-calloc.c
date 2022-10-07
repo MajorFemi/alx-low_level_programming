ca2bf4854cd84d0b8d1c1049b108c3c2f37b8e82
MajorFemi <harlaohorluwafemi@gmail.com>
2022-10-06 18:47:26 -0700

    third

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _calloc - allocates memory for an array
+ * @nmemb: number of elements
+ * @size: size of each element
+ * Return: Null if fails, else pointer to alloc memory
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *f;
+	unsigned int m;
+
+	if (nmemb <= 0 || size <= 0)
+		return (NULL);
+
+	f = malloc(nmemb * size);
+	if (f == NULL)
+		return (NULL);
+	for (m = 0; m < nmemb; m++)
+		f[m] = 0;
+
+	return (f);
+}
