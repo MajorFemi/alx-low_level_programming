d282696f64ed3ac17c92e871d3b22cd8fbd78a35
MajorFemi <harlaohorluwafemi@gmail.com>
2022-10-10 18:56:21 -0700

    third commit

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,10 +12,14 @@ void print_dog(struct dog *d)
 {
 	if (d)
 	{
-		printf("Name: ");
-		d->name == NULL ? printf("nil)\n") : printf("%s\n", d->name);
+		if (d->name != NULL)
+			printf("Name: %s/n", d->name);
+		else
+			printf("Name: (nil)\n");
 		printf("Age: %f\n", d->age);
-		printf("Owner: ");
-		d->owner == NULL ? printf("(nil)\n") : printf("%s\n", d->owner);
+		if (d->owner)
+			printf("Owner: %s\n", d->owner);
+		else
+			printf("Owner: (nil)\n");
 	}
 }
