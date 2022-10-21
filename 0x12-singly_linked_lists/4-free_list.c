997ed96286375c6c45af4b82f37460c9226dfe10
MajorFemi <harlaohorluwafemi@gmail.com>
2022-10-20 20:17:16 -0700

    fifth commit

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_list = frees all elements in linked list
+ * @head: head
+ * Return: void
+ */
+
+void free_list(list_t *head)
+{
+	list_t *tempy;
+
+	tempy = head;
+	while (head)
+	{
+		tempy = head;
+		head = head->next;
+		free(tempy->str);
+		free(tempy);
+	}
+	return (head);
+}
