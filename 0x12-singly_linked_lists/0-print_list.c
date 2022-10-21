80b8b06b6a230ca79024d0f81318d992d225e77f
MajorFemi <harlaohorluwafemi@gmail.com>
2022-10-20 19:11:43 -0700

    first commit

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - print all element of list_t list
+ * @h: head of linked list
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	const list_t *present;
+	size_t f;
+
+	f = 0;
+	present = h;
+	while (present != NULL)
+	{
+		printf("[%d] %s\n", present->len, present->str);
+		present = present->next;
+		f++;
+	}
+
+	return (c)
+}
