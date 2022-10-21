2bfa6c5d21d2cba94081000fadd8d3386b628ab8
MajorFemi <harlaohorluwafemi@gmail.com>
2022-10-20 20:00:45 -0700

    third commit

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <string.h>
+
+/**
+ * add_node - adds node to the linked list head
+ * @head: head
+ * @str: string
+ * Return: pointer to new head of list, NULL on failure
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *tempy;
+	int length = 0;
+
+	tempy = malloc(sizeof(list_t));
+	if (tempy == NULL)
+		return NULL;
+
+	while (str[length])
+		length++;
+
+	tempy->len = length;
+	tempy->str = strdup(str);
+	tempy->next = *head;
+	*head = tempy;
+	return (tempy);
+}
