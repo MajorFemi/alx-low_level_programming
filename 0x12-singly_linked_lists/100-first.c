0b1ad7c52eab4025bdc9481dd9b4783b7fbb0a46
MajorFemi <harlaohorluwafemi@gmail.com>
2022-10-20 20:43:24 -0700

    sixth commit

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,15 +2,12 @@
 
 /* Using Constructors in c */
 
-void main_constructor(void) __attribut__((constructor));
 /**
- * main_constructor - constructor function that print messsage before
- * main runs
- * Return: void
+ * before_main - print statement within this function before running main
  */
 
-void main_constructor(void)
+void __attribute__((constructor)) before_main(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon mu back!\n");
+	printf("I bore my house upon my back!\n");
 }
