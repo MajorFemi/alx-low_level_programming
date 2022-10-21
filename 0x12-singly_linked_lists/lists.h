bfd1323660aecb6a01b42f811bf0b49baa39a887
MajorFemi <harlaohorluwafemi@gmail.com>
2022-10-20 19:24:42 -0700

    first commit

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,14 +1,15 @@
-#ifndef LIST_H
-#define LIST_H
+#ifndef LISTS_H
+#define LISTS_H
 
 #include <stdlib.h>
 
 /**
  * struct list_s - singly linked list
- * @str: string
+ * @str: string - (malloc'ed string)
  * @len: length of string
  * @next: pointd to the next node
  *
+ * Description: singly linked node structure
  */
 
 typedef struct list_s
@@ -18,6 +19,7 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
+int _putchar(char c);
 size_t print_list(const list_t *h);
 
-#endif /* LIST_H */
+#endif /* LISTS_H */
