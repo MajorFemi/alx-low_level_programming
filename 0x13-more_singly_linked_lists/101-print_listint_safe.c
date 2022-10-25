ca9fb6979f375604aafbd82c703648833803ba89
MajorFemi <harlaohorluwafemi@gmail.com>
2022-10-25 10:33:08 -0700

    Print

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -11,13 +11,13 @@ size_t print_listint_safe(const listint_t *head)
 	size_t nodes = 0;
 	const listint_t *print, *tempy;
 
-	tempy = head
+	tempy = head;
 	if (tempy == NULL)
 		exit(98);
 
 	while (tempy != NULL)
 	{
-		print = tempy
+		print = tempy;
 		tempy = tempy->next;
 		nodes;
 		printf("[%p] %d\n", (void *)print, print->n);
