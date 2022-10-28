6c3f35c8986cdc85b5b8f7c811b42253ddef3c14
MajorFemi <harlaohorluwafemi@gmail.com>
2022-10-27 18:09:56 -0700

    get endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * get_endianness - fnction checks for endianness
+ * Return: 0 if big endian otherwise 1
+ */
+
+int get_endianness(void)
+{
+	unsigned int f = 1;
+	char *e = (char *)&f;
+
+	if (*a)
+		return (1);
+	else
+		return (0);
+}
