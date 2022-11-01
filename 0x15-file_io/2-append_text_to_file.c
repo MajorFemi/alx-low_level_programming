86468d77c0c9cd608e1af4e4addd566773f25340
MajorFemi <harlaohorluwafemi@gmail.com>
2022-10-31 18:05:59 -0700

    Speak gently, she can hear

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,40 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/uio.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+/**
+ * append_text_to_file - appends text at the end
+ * @filename: filename to open and append
+ * @text_content: NULL terminated string to add
+ * Return: 1 on sucess, -1 if the file can not be created, written
+ * nor fails
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fdop, fdwr, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	fdop = open(filename, O_RDWR | O_APPEND);
+	if (fdop < 0)
+		return (-1);
+	if (text_content == NULL)
+	{
+		close(fdop);
+		return (1);
+	}
+
+	while (*text_content + len)
+		len++;
+
+	fdwr = write(fdop, text_content, len);
+	close(fdop);
+	if (fdwr < 0)
+		return (-1);
+
+	return (1);
+}
