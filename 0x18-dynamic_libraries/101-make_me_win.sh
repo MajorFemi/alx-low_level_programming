b3bdee400bca6f3e11f6ce9c0facb065ed72b035
MajorFemi <harlaohorluwafemi@gmail.com>
2022-12-16 16:40:39 -0800

    All

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/MajorFemi/alx-low_level_programming/master/0x18-dynamic_libraries/cheat.so
-export LD_PRELOAD=/tmp/cheat.so
+wget -P /tmp/ https://raw.github.com/MajorFemi/alx-low_level_programming/master/0x18-dynamic_libraries/libwincheat.so
+export LD_PRELOAD=/tmp/libwincheat.so
